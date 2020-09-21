21783420863910babd308696ddf7e4c37a24c270
n1ckDotEXE <admin@nicholasnam.com>
2020-09-21 14:06:56 -0400

    Complete

--- main.test.js
+++ main.test.js
@@ -2,8 +2,37 @@
  * YOUR CODE HERE *
  ******************/
 
+let greeting = 'Hello';
+let sum = 0;
+let prod = 0;
 
+function greet (value) {
+  greeting = greeting + ' ' + value;
+}
 
+function sumOfTwo (a, b) {
+  sum = a + b;
+}
+
+function multiply (a, b, c) {
+  prod = a * b * c;
+}
+
+function sayHi(name) {
+  return 'Hello ' + name + '!';
+}
+
+function returnWhatISay(value) {
+  return value;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function remainder (a, b) {
+  return a % b;
+}
 
 /*********************************************************************************************************************
 
